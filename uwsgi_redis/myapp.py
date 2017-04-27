e339be0530762aaa25ec577069abdb6e98cd022f
Ubuntu <ubuntu@ip-172-31-7-254.us-west-2.compute.internal>
2017-04-27 05:12:32 +0000

    JavaScript and Flask!

--- uwsgi_redis/myapp.py
+++ uwsgi_redis/myapp.py
@@ -1,22 +1,41 @@
 import redis
-from flask import Flask
-application = Flask(__name__)
+from flask import Flask, render_template
+app = application = Flask(__name__)
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+        return render_template('hello.html', name=name)
 
 @application.route("/")
-def hello():
+def index():
     r = redis.Redis('redis')
     try:
         r.get('counter')
     except:
         r.set('counter', 0)
     r.incr('counter')
-    return "<h1>This page has been visited %s times!</h1>" % r.get('counter')
+    counter = r.get('counter')
+    return render_template("index.html", counter=counter)
+    #return "<h1>This page has been visited %s times!</h1>" % r.get('counter')
 
 @application.route("/clear")
 def blank():
     r = redis.Redis('redis')
     r.set('counter', 0)
     return "<h1>CLEARED COUNTER TO: %s</h1>" % r.get('counter')
+
+@application.route("/fetch")
+def fetch():
+        return '''[
+        {"url": "http://www.ucla.edu", "display": "UCLA"},
+        {"url": "http://www.usc.edu", "display": "USC"}]
+        '''
+@application.route("/increment")
+def incre():
+        r = redis.Redis('redis')
+        r.incr('counter')
+        return r.get('counter')
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
